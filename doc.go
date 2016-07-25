120539f69252fe4e81e43999324f90add330c619
Andras Belicza <iczaaa@gmail.com>
2016-07-25 13:49:03 +0200

    Extended godoc with examples; formatting.

--- doc.go
+++ doc.go
@@ -4,22 +4,50 @@ Package mpq is a decoder/parser of Blizzard's MPQ archive file format.
 
 This is not a full implementation, primarily intended to parse StarCraft II replay files (*.SC2Replay).
 
-Information sources:
+Usage
 
-- The_MoPaQ_Archive_Format: http://wiki.devklog.net/index.php?title=The_MoPaQ_Archive_Format
+Usage is simple. Opening an MPQ archive file:
 
-- MPQ on wikipedia: http://en.wikipedia.org/wiki/MPQ
+	m, err := mpq.NewFromFile("myreplay.SC2Replay")
+	if err != nil {
+		// Handle error
+		return
+	}
+	defer m.Close()
 
-- Zezula MPQ description: http://www.zezula.net/mpq.html
+Getting a named file from the archive:
 
-- Stormlib: https://github.com/ladislav-zezula/StormLib
+	// Access a file inside the MPQ archive.
+	// Usually there is a file called "(listfile)" containing the list of other files:
+	if data, err := m.FileByName("(listfile)"); err == nil {
+		fmt.Println("Files inside archive:")
+		fmt.Println(string(data))
+	} else {
+		// handle error
+	}
 
-- Libmpq project: https://github.com/ge0rg/libmpq (old: https://libmpq.org/)
+If you already have the MPQ data in memory:
 
-- MPQ parser of the Scelight project: https://github.com/icza/scelight/tree/master/src-app-libs/hu/belicza/andras/mpq
+	mpqdata := []byte{/* ... */} // MPQ data in memory
+	m, err := mpq.New(bytes.NewReader(mpqdata)))
+
+
+Information sources
+
+The_MoPaQ_Archive_Format: http://wiki.devklog.net/index.php?title=The_MoPaQ_Archive_Format
+
+MPQ on wikipedia: http://en.wikipedia.org/wiki/MPQ
+
+Zezula MPQ description: http://www.zezula.net/mpq.html
+
+Stormlib: https://github.com/ladislav-zezula/StormLib
+
+Libmpq project: https://github.com/ge0rg/libmpq (old: https://libmpq.org/)
+
+MPQ parser of the Scelight project: https://github.com/icza/scelight/tree/master/src-app-libs/hu/belicza/andras/mpq
 
 Format of the "(attributes)" meta attributes file:
-href="https://github.com/stormlib/StormLib/blob/3a926f0228c68d7d91cf3946624d7859976440ec/src/SFileAttributes.cpp
+https://github.com/stormlib/StormLib/blob/3a926f0228c68d7d91cf3946624d7859976440ec/src/SFileAttributes.cpp
 
     int version: Version of the (attributes) file. Must be 100 (0x64)
     int flags: flags telling what is contained in the "(attributes)"
@@ -30,8 +58,11 @@ href="https://github.com/stormlib/StormLib/blob/3a926f0228c68d7d91cf3946624d7859
         MPQ_ATTRIBUTE_ALL           0x0000000F  Summary mask
 
 If has CRC32: int * BlockTableSize
+
 If has FILETIME: long * BlockTableSize
+
 If has MD5: MD5SIZE * BlockTableSize
+
 If has PATCH_BIT: enough bytes to hold BlockTableSize bits
 
 */
